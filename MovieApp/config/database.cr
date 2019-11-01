3da24c2abdeb183d2ef2cfb9a2580400fddbccbe
jowisaidmajn <joelsaidman1@gmail.com>
2019-11-01 17:01:07 -0300

    limpio repo

--- MovieApp/config/database.cr
+++
@@ -1,5 +0,0 @@
-require "granite/adapter/pg"
-
-Granite::Connections << Granite::Adapter::Pg.new(name: "pg", url: Amber.settings.database_url)
-Granite.settings.logger = Amber.settings.logger.dup
-Granite.settings.logger.not_nil!.progname = "Granite"
